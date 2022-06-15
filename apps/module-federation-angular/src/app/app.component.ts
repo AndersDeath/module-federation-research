3b4318a2b75e12cbe59383ed5faaa9cdf26ca8fe
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-06-15 11:42:34 +0400

    init

---
+++ apps/module-federation-angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'module-federation-research-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'module-federation-angular';
+}
